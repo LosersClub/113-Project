0039ad2f13cbb7438bb912e362e3ebdbfebed1a3
rajeevgUCI <rajeevg@uci.edu>
2018-10-14 01:12:52 -0700

    Add singleton tests for first Instance and destroy

--- Assets/Tests/Runtime/core/SingletonTests.cs
+++ Assets/Tests/Runtime/core/SingletonTests.cs
@@ -9,32 +9,41 @@ public class SingletonTests {
   // another. So, they have to be put together into this one test method:
   [Test]
   public void Singleton_CreatesAndDestroysDupesAndDestroysInstance() {
-    GameObject firstComponentObj = new GameObject();
-    firstComponentObj.AddComponent<SingletonTestComponent>();
-    Assert.That(firstComponentObj.GetComponent<SingletonTestComponent>(),
-                  Is.EqualTo(SingletonTestComponent.Instance));
+    // Test that can create a new component on first Instance call:
+    SingletonTestComponent firstInstance = SingletonTestComponent.Instance;
 
-    GameObject toDestroyComponentObj = new GameObject();
+    GameObject toDestroyObject = new GameObject();
+    toDestroyObject.AddComponent<SingletonTestComponent>();
     // Adding the component creates a second component, which should get
     // destroyed by SingletonMonoBehavior. But it won't get destroyed
     // immediately, so before it is destroyed, make sure that the singleton
     // Instance wasn't changed to the new component:
-    toDestroyComponentObj.AddComponent<SingletonTestComponent>();
-    // Make sure that Instance wasn't changed to the second component:
-    Assert.That(toDestroyComponentObj.GetComponent<SingletonTestComponent>(),
+    Assert.That(toDestroyObject.GetComponent<SingletonTestComponent>(),
                   Is.Not.EqualTo(SingletonTestComponent.Instance));
-    // Make sure the Instance is still the first component:
-    Assert.That(firstComponentObj.GetComponent<SingletonTestComponent>(),
-                  Is.EqualTo(SingletonTestComponent.Instance));
+    // Make sure the Instance is still the first instance:
+    Assert.That(firstInstance, Is.EqualTo(SingletonTestComponent.Instance));
     // Finally, make sure that the component was not already destroyed. If it
     // was, then the tests above are inaccurate:
-    Assert.That(toDestroyComponentObj.GetComponent<SingletonTestComponent>(),
+    Assert.That(toDestroyObject.GetComponent<SingletonTestComponent>(),
                 Is.Not.Null);
 
-    // TODO: wait until toDestroyComponentObj is destroyed. Then, destroy
-    // firstComponentObj, and wait for it to be destroyed. Then, test that
-    // Instance was also destroyed, and that attempting to access Instance
-    // raises Exception. Note that calling UnityEngine.Object.Destroy(obj) will
-    // not immediately destroy obj; this complicates testing.
+    // TODO: wait until toDestroyObject's SingletonTestComponent is destroyed.
+    // Have to wait because SingletonTestComponent calls
+    // UnityEngine.Object.Destroy to destroy the component, but that method
+    // does not immediately destroy the component. For now, instead of waiting,
+    // destroy the object immediately here, and then proceed with testing:
+    UnityEngine.Object.DestroyImmediate(toDestroyObject.GetComponent<SingletonTestComponent>());
+    // Make sure that the component was destroyed:
+    Assert.That(toDestroyObject.GetComponent<SingletonTestComponent>(),
+                Is.Null);
+
+    var firstInstanceObj = firstInstance.gameObject;
+    UnityEngine.Object.DestroyImmediate(firstInstance);
+    // Have to test that firstInstanceObj.GetComponent<SingletonTestComponent>()
+    // Is.Null. Testing firstInstance Is.Null fails, stating "Expected null but
+    // was <null>" for some reason.
+    Assert.That(firstInstanceObj.GetComponent<SingletonTestComponent>(), Is.Null);
+
+    Assert.That(() => { return SingletonTestComponent.Instance; }, Throws.ArgumentException);
   }
 }
