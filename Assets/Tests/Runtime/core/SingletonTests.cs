95d4e51f719a664edf2b3eb1346f95f68136f081
rajeevgUCI <rajeevg@uci.edu>
2018-10-14 12:25:18 -0700

    Shorten line length in SingletonTests

--- Assets/Tests/Runtime/core/SingletonTests.cs
+++ Assets/Tests/Runtime/core/SingletonTests.cs
@@ -13,37 +13,43 @@ public class SingletonTests {
     SingletonTestComponent firstInstance = SingletonTestComponent.Instance;
     Assert.That(firstInstance, Is.Not.Null);
 
-    GameObject toDestroyObject = new GameObject();
-    toDestroyObject.AddComponent<SingletonTestComponent>();
+    GameObject toDestroyObj = new GameObject();
+    toDestroyObj.AddComponent<SingletonTestComponent>();
     // Adding the component creates a second component, which should get
     // destroyed by SingletonMonoBehavior. But it won't get destroyed
     // immediately, so before it is destroyed, make sure that the singleton
     // Instance wasn't changed to the new component:
-    Assert.That(toDestroyObject.GetComponent<SingletonTestComponent>(),
+    Assert.That(toDestroyObj.GetComponent<SingletonTestComponent>(),
                   Is.Not.EqualTo(SingletonTestComponent.Instance));
     Assert.That(firstInstance, Is.EqualTo(SingletonTestComponent.Instance));
     // Make sure that the component was not already destroyed. If it was, then
     // the tests above are inaccurate:
-    Assert.That(toDestroyObject.GetComponent<SingletonTestComponent>(),
+    Assert.That(toDestroyObj.GetComponent<SingletonTestComponent>(),
                 Is.Not.Null);
 
-    // TODO: wait until toDestroyObject's SingletonTestComponent is destroyed.
+    // TODO: wait until toDestroyObj's SingletonTestComponent is destroyed.
     // Have to wait because SingletonTestComponent uses
     // UnityEngine.Object.Destroy, which does not immediately destroy the
     // component.
     // Until that TODO is complete, destroy the object immediately to proceed
     // with testing:
-    UnityEngine.Object.DestroyImmediate(toDestroyObject.GetComponent<SingletonTestComponent>());
-    Assert.That(toDestroyObject.GetComponent<SingletonTestComponent>(),
+    var toDestroyComp = toDestroyObj.GetComponent<SingletonTestComponent>();
+    UnityEngine.Object.DestroyImmediate(toDestroyComp);
+    // Note: have to test via toDestroyObj.GetComponent. Directly testing
+    // toDestroyComp Is.Null fails, stating "Expected null but was <null>" for
+    // some reason.
+    Assert.That(toDestroyObj.GetComponent<SingletonTestComponent>(),
                 Is.Null);
 
-    var firstInstanceObj = firstInstance.gameObject;
+    GameObject firstInstanceObj = firstInstance.gameObject;
     UnityEngine.Object.DestroyImmediate(firstInstance);
     // Note: have to test via firstInstanceObj.GetComponent. Directly testing
     // firstInstance Is.Null fails, stating "Expected null but was <null>" for
     // some reason.
-    Assert.That(firstInstanceObj.GetComponent<SingletonTestComponent>(), Is.Null);
+    Assert.That(firstInstanceObj.GetComponent<SingletonTestComponent>(),
+                Is.Null);
 
-    Assert.That(() => { return SingletonTestComponent.Instance; }, Throws.ArgumentException);
+    Assert.That(() => { return SingletonTestComponent.Instance; },
+                Throws.ArgumentException);
   }
 }
