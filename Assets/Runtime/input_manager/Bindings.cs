2d94be75aa67be697734770f92739670efe610a1
rajeevgUCI <rajeevg@uci.edu>
2018-10-15 19:23:46 -0700

    Add initial platform fall through

--- Assets/Runtime/input_manager/Bindings.cs
+++ Assets/Runtime/input_manager/Bindings.cs
@@ -8,6 +8,11 @@ public class Bindings {
       new KeyboardButton(KeyCode.D));
     InputManager.AddButtonAction("Left", () => GameManager.Player.Move(-1.0f),
       new KeyboardButton(KeyCode.A));
+    InputManager.AddButtonAction("Down", GameManager.Player.DropDown,
+      new KeyboardButton(KeyCode.S));
+    // TODO: set EndDropDown key to S release:
+    InputManager.AddButtonAction("DownRelease", GameManager.Player.EndDropDown,
+      new KeyboardButton(KeyCode.Space));
     InputManager.AddButtonAction("Jump", GameManager.Player.Jump,
       new KeyboardButton(KeyCode.Space));
     InputManager.AddAnalog2DAction("Controller move", (float x, float y) => GameManager.Player.Move(x),
