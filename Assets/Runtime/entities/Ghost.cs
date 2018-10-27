320762444c2b5a762ccd26a88f5a82d37cb0f00e
rajeevgUCI <rajeevg@uci.edu>
2018-10-26 20:56:19 -0700

    Add BoxCollider and RigidBody to Ghost

--- Assets/Runtime/entities/Ghost.cs
+++ Assets/Runtime/entities/Ghost.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ghost : MonoBehaviour {
 
+  private BoxCollider2D boxCollider2D;
+  private Rigidbody2D rigidBody2D;
+
   void Start () {
-    
+    this.boxCollider2D = this.GetComponent<BoxCollider2D>();
+    this.rigidBody2D = this.GetComponent<Rigidbody2D>();
   }
   
   void Update () {
-    
+
   }
 }
