700bb77b32d84054cb0d05346997c0e893d3fa1f
rajeevgUCI <rajeevg@uci.edu>
2018-10-27 15:05:20 -0700

    Move GhostTestScene into subdir, add camera control

---
+++ Assets/Scenes/GhostTest/GhostTestCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTestCamera : MonoBehaviour {
+
+	public float moveSpeedMultiplier = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = this.transform.position + moveSpeedMultiplier * new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+	}
+}
