3373f86352ca61a33d6a612932c6745cfb946efe
Kyle Bartz <kyle@unleashurgeek.com>
2018-10-27 20:48:10 -0700

    Created base DamageDealer and DamageTaker classes

---
+++ Assets/Runtime/entities/damage/DamageDealer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public abstract class DamageDealer : MonoBehaviour {
+  [SerializeField]
+  protected float damage = 1.0f;
+
+  [SerializeField]
+  protected bool disableDamageAfterHit;
+  [SerializeField]
+  protected bool ignoreInvulnerability;
+  [SerializeField]
+  protected LayerMask hittableLayers;
+
+  [SerializeField]
+  protected DamageEvent OnDamageHit;
+  [SerializeField]
+  protected NonDamageEvent OnNoDamageHit;
+
+  public abstract Collider2D LastHit { get; }
+  private bool canDealDamage = true;
+  public bool CanDealDamage {
+    get { return canDealDamage; }
+    set { this.canDealDamage = value; }
+  }
+
+  public float Damage { get { return this.damage; } }
+  public bool IgnoreInvulnerability { get { return this.ignoreInvulnerability; } }
+
+  protected virtual void PerformHit(DamageTaker objHit) {
+    if (objHit) {
+      this.OnDamageHit.Invoke(this, objHit);
+      objHit.TakeDamage(this, this.ignoreInvulnerability);
+      if (this.disableDamageAfterHit) {
+        this.CanDealDamage = false;
+      }
+    } else {
+      this.OnNoDamageHit.Invoke(this);
+    }
+  }
+}
---
+++ Assets/Runtime/entities/damage/DamageTaker.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class DamageTaker : MonoBehaviour {
+  [SerializeField, ReadOnly]
+  protected float currentHealth;
+  [SerializeField]
+  protected float maxHealth = 5.0f;
+  [SerializeField]
+  protected bool invulnerableAfterDamage = true;
+  [SerializeField]
+  protected float invulnerabilityDuration = 1f;
+  [SerializeField]
+  protected bool disableOnDeath = false;
+
+  [SerializeField]
+  protected HealEvent OnGainHealth;
+  [SerializeField]
+  protected DamageEvent OnTakeDamage;
+  [SerializeField]
+  protected DamageEvent OnDie;
+
+  protected Vector2 damageDirection;
+  protected bool invulnerable;
+  protected float invulnerableTimer;
+
+  public float Health { get { return this.currentHealth; } }
+  public bool Invulnerable { get { return this.invulnerable; } }
+  public Vector2 DamageDirection { get { return this.damageDirection; } }
+
+  protected virtual void OnEnable() {
+    this.currentHealth = this.maxHealth;
+    this.invulnerable = false;
+  }
+
+  protected virtual void Update() {
+    if (invulnerable) {
+      invulnerableTimer -= Time.deltaTime;
+      if (invulnerableTimer <= 0f) {
+        this.invulnerable = false;
+      }
+    }
+  }
+
+  public void EnableInvulnerability(bool noTimer = false) {
+    this.invulnerable = true;
+    this.invulnerableTimer = noTimer ? float.MaxValue : this.invulnerabilityDuration;
+  }
+
+  public void TakeDamage(DamageDealer dealer, bool ignoreInvincible = false) {
+    if ((this.invulnerable && !ignoreInvincible) || currentHealth <= 0) {
+      return;
+    }
+
+    if (!invulnerable) {
+      this.currentHealth -= dealer.Damage;
+      this.EnableInvulnerability();
+    }
+
+    this.damageDirection = this.transform.position - dealer.transform.position;
+    this.OnTakeDamage.Invoke(dealer, this);
+
+    if (this.currentHealth <= 0) {
+      this.OnDie.Invoke(dealer, this);
+      if (this.disableOnDeath) {
+        this.gameObject.SetActive(false);
+      }
+    }
+  }
+
+  public void GainHealth(float amount) {
+    this.currentHealth += amount;
+    if (this.currentHealth > this.maxHealth) {
+      this.currentHealth = this.maxHealth;
+    }
+    OnGainHealth.Invoke(amount, this);
+  }
+}
