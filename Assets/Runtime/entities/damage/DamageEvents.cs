e746c2860918922162d1636f32693deaccc3cda0
Kyle Bartz <kyle@unleashurgeek.com>
2018-10-27 20:43:57 -0700

    Created Unity events to hook for the damage system

---
+++ Assets/Runtime/entities/damage/DamageEvents.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.Events;
+
+[Serializable]
+public class DamageEvent : UnityEvent<DamageDealer, DamageTaker> { }
+
+[Serializable]
+public class NonDamageEvent : UnityEvent<DamageDealer> { }
+
+[Serializable]
+public class HealEvent : UnityEvent<float, DamageTaker> { }
