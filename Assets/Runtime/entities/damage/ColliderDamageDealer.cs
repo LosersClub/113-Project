28bef52cd15ed0548e8f4961fb417e829809131b
Kyle Bartz <kyle@unleashurgeek.com>
2018-10-27 20:48:51 -0700

    Created Colider2D based damage dealer

---
+++ Assets/Runtime/entities/damage/ColliderDamageDealer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ColliderDamageDealer : DamageDealer {
+
+  public bool UseBaseCollider = true;
+  private BoxCollider2D damageCollider;
+
+  private readonly Collider2D[] hits = new Collider2D[10];
+  private ContactFilter2D contactFilter;
+
+  private Collider2D lastHit;
+  public override Collider2D LastHit {
+    get { return this.lastHit; }
+  }
+
+  private void Awake() {
+    if (this.UseBaseCollider) {
+      this.damageCollider = this.GetComponent<BoxCollider2D>();
+    }
+    this.contactFilter.layerMask = this.hittableLayers;
+    this.contactFilter.useLayerMask = true;
+    this.contactFilter.useTriggers = true;
+  }
+
+  private void Update() {
+    if (!this.CanDealDamage) {
+      return;
+    }
+
+    int count = Physics2D.OverlapCollider(this.damageCollider, this.contactFilter, this.hits);
+    for (int i = 0; i < count; i++) {
+      this.lastHit = this.hits[i];
+      this.PerformHit(this.lastHit.GetComponent<DamageTaker>());
+    }
+  }
+}
