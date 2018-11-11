810b9b5152de7fd9deb73fed82a1fefd5cc554a9
Kyle Bartz <kyle@unleashurgeek.com>
2018-11-11 12:30:43 -0800

    Switched ColliderDamageDealer to use any Collider2D

--- Assets/Runtime/entities/damage/ColliderDamageDealer.cs
+++ Assets/Runtime/entities/damage/ColliderDamageDealer.cs
@@ -4,7 +4,7 @@ public class ColliderDamageDealer : DamageDealer {
 
   public bool UseBaseCollider = true;
   [SerializeField, ConditionalHide("UseBaseCollider", inverse: true)]
-  private BoxCollider2D damageCollider;
+  private Collider2D damageCollider;
 
   private readonly Collider2D[] hits = new Collider2D[10];
   private ContactFilter2D contactFilter;
@@ -16,7 +16,7 @@ public class ColliderDamageDealer : DamageDealer {
 
   private void Awake() {
     if (this.UseBaseCollider) {
-      this.damageCollider = this.GetComponent<BoxCollider2D>();
+      this.damageCollider = this.GetComponent<Collider2D>();
     }
     this.contactFilter.layerMask = this.hittableLayers;
     this.contactFilter.useLayerMask = true;
