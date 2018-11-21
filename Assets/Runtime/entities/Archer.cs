3b393a9a03860d6965b49a8e3c7e67c491e02ead
rajeevgUCI <rajeevg@uci.edu>
2018-11-20 21:02:09 -0800

    Archer: add facing direction

--- Assets/Runtime/entities/Archer.cs
+++ Assets/Runtime/entities/Archer.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Archer : MonoBehaviour {
 
   [SerializeField]
   private GameObject arrowPrefab;
   [SerializeField]
   private float arrowFiringInterval = 4.0f;
+  [SerializeField]
+  private bool isInitiallyFacingRight = true;
+
+  private SpriteRenderer spriteRenderer;
+
+  private bool isFacingRight;
 
   void Start () {
     Assert.IsNotNull(arrowPrefab);
+
+    this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+
+    this.isFacingRight = this.isInitiallyFacingRight;
 
     StartCoroutine(FireArrowsCoroutine());
   }
   
   void Update () {
-
+    if(this.isFacingRight && this.spriteRenderer.flipX) {
+      this.spriteRenderer.flipX = false;
+    }
+    else if(!this.isFacingRight && !this.spriteRenderer.flipX) {
+      this.spriteRenderer.flipX = true;
+    }
   }
 
   private IEnumerator FireArrowsCoroutine() {
