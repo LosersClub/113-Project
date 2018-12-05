0b12d5a247f9c054c7800f453cdb439b1c0d5eb1
rajeevgUCI <rajeevg@uci.edu>
2018-12-04 17:31:37 -0800

    Archer: refactor to use EnemyComponent

--- Assets/Runtime/entities/Archer.cs
+++ Assets/Runtime/entities/Archer.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-[RequireComponent(typeof(MovementController))]
 [RequireComponent(typeof(CameraBoundsChecker))]
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(EnemyComponent))]
 public class Archer : MonoBehaviour {
 
   [SerializeField]
@@ -15,35 +14,27 @@ public class Archer : MonoBehaviour {
   [SerializeField]
   private Vector2 arrowOffset = new Vector2(0.25f, 0.5f);
 
-  private MovementController movementController;
   private CameraBoundsChecker cameraBoundsChecker;
-  private Animator animator;
-
-  private bool isFacingRight = true; // will be changed to face player on first Update()
-  private bool inAction = false;
+  private EnemyComponent enemyComponent;
 
   void Start () {
     Assert.IsNotNull(arrowPrefab);
 
-    this.movementController = this.GetComponent<MovementController>();
     this.cameraBoundsChecker = this.GetComponent<CameraBoundsChecker>();
-    this.animator = this.GetComponent<Animator>();
+    this.enemyComponent = this.GetComponent<EnemyComponent>();
   }
   
   void Update () {
-    this.FacePlayer();
-
-    Vector2 newVelocity = this.movementController.Velocity + Physics2D.gravity * Time.deltaTime;
-    this.movementController.Move(newVelocity * Time.deltaTime);
+    this.enemyComponent.LookAtTarget();
   }
 
   public void OnAnimatorStateEnter(AnimatorStateInfo stateInfo) {
     if(stateInfo.IsName("Idle")) {
-      this.inAction = false;
+      this.enemyComponent.inAction = false;
       StartCoroutine(WaitThenFireOnceCoroutine());
     }
     else if(stateInfo.IsName("Fire Tell")) {
-      this.inAction = true;
+      this.enemyComponent.inAction = true;
     }
     else if(stateInfo.IsName("Fire")) {
       this.FireArrow();
@@ -56,25 +47,15 @@ public class Archer : MonoBehaviour {
       StartCoroutine(WaitThenFireOnceCoroutine());
     }
     else {
-      this.animator.SetTrigger("Fire Tell");
+      this.enemyComponent.Anim.SetTrigger("Fire Tell");
       // Arrow prefab is fired later, when animator state changes to Fire.
     }
   }
 
   private void FireArrow() {
-    Vector3 arrowPosition = this.transform.position + new Vector3(this.arrowOffset.x * (this.isFacingRight ? 1 : -1), this.arrowOffset.y, 0);
+    Vector3 arrowPosition = this.transform.position + new Vector3(this.arrowOffset.x * (this.enemyComponent.FacingRight ? 1 : -1), this.arrowOffset.y, 0);
     GameObject arrow = Instantiate(arrowPrefab, arrowPosition, Quaternion.identity);
     Vector2 fireDirection = GameManager.Player.transform.position - this.transform.position;
     arrow.GetComponent<ArcherArrow>().Fire(fireDirection);
-  }
-
-  private void FacePlayer() {
-    bool newFacingRight = GameManager.Player.transform.position.x > this.transform.position.x;
-
-    if(newFacingRight != this.isFacingRight) {
-      this.transform.localScale = new Vector3(this.transform.localScale.x * -1, this.transform.localScale.y, this.transform.localScale.z);
-    }
-
-    this.isFacingRight = newFacingRight;
   }
 }
