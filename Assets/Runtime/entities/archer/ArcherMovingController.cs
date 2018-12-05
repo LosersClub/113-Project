200edaedbe67e00b0653886b99826aeddbf4e7ba
rajeevgUCI <rajeevg@uci.edu>
2018-12-04 19:32:56 -0800

    Add ArcherMoving

---
+++ Assets/Runtime/entities/archer/ArcherMovingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyComponent))]
+[RequireComponent(typeof(ChaseComponent))]
+public class ArcherMovingController : MonoBehaviour, IArcherFireChecker {
+
+  private EnemyComponent enemyComponent;
+  private ChaseComponent chaseComponent;
+
+  void Start () {
+    this.enemyComponent = this.GetComponent<EnemyComponent>();
+    this.chaseComponent = this.GetComponent<ChaseComponent>();
+  }
+  
+  void Update () {
+    this.enemyComponent.LookAtTarget();
+  }
+
+  public bool CanFire() {
+    return this.chaseComponent.WithinStopDistance();
+  }
+}
