9ee9a66d016e514970e365769bbef63c6f532a37
rajeevgUCI <rajeevg@uci.edu>
2018-12-08 23:14:03 -0800

    Chalice: control beam fire from script

--- Assets/Runtime/entities/chalice/Chalice.cs
+++ Assets/Runtime/entities/chalice/Chalice.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Chalice : MonoBehaviour {
 
+  [SerializeField]
+  private GameObject beamEmitterObject;
+
+  private BeamEmitter beamEmitterComponent;
+
   void Start () {
-    
+    Assert.IsNotNull(this.beamEmitterObject);
+
+    this.beamEmitterComponent = this.beamEmitterObject.GetComponent<BeamEmitter>();
+    Assert.IsNotNull(this.beamEmitterComponent);
+
+    StartCoroutine(FireCoroutine());
   }
-  
+
   void Update () {
-    
+
+  }
+
+  private IEnumerator FireCoroutine() {
+    while(true) {
+      yield return new WaitForSeconds(3);
+      this.beamEmitterComponent.Fire();
+      yield return new WaitUntil(() => !this.beamEmitterComponent.IsFiring);
+    }
   }
 }
