d170a3194180d36ec70ff3862ea3384b567103d8
Kyle Bartz <kyle@unleashurgeek.com>
2018-12-09 11:00:59 -0800

    Created Simple level implementation

---
+++ Assets/Runtime/level_gen/SimpleLevel.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+public class SimpleLevel : Level {
+
+  [Header("Simple Settings")]
+  public int roomCount = 3;
+  public Vector2Int roomSize = new Vector2Int(28, 16);
+
+  public bool manualLastRoom = false;
+  [ConditionalHide("manualLastRoom")]
+  public ManualRoom lastRoom;
+  public CustomRoom[] customRooms;
+
+  [Header("Prefabs")]
+  public GenericPool ground;
+  public GenericPool groundFiller;
+  public GenericPool leftPlatform;
+  public GenericPool mainPlatform;
+  public GenericPool rightPlaform;
+
+  public override Room[] GenerateRooms() {
+    Room[] rooms = new Room[this.roomCount];
+    this.ground.Init(this.gameObject);
+    this.groundFiller.Init(this.gameObject);
+    this.leftPlatform.Init(this.gameObject);
+    this.mainPlatform.Init(this.gameObject);
+    this.rightPlaform.Init(this.gameObject);
+
+    for (int i = 0; i < roomCount; i++) {
+      if (manualLastRoom && i == roomCount - 1) {
+        rooms[i] = lastRoom.Get();
+        continue;
+      }
+
+      foreach (CustomRoom cr in this.customRooms) {
+        if (cr.index == i) {
+          rooms[i] = cr.room.Get();
+          continue;
+        }
+      }
+
+      // TODO: Call Room Generator if MonoRoom doesnt exist
+
+      rooms[i] = new Room(28, 16);
+      rooms[i].AddGround();
+      rooms[i].AddPlatform(new Platform(new Vector2(4.5f, 4.5f), 6));
+    }
+
+    return rooms;
+  }
+
+  public override void RenderRoom(Room room) {
+    for (int i = 1; i <= 2; i++) {
+      this.ground.Pop(new Vector3(-i, 1));
+      this.groundFiller.Pop(new Vector3(-i, 0));
+      this.ground.Pop(new Vector3(room.Width - 1 + i, 1));
+      this.groundFiller.Pop(new Vector3(room.Width - 1 + i, 0));
+    }
+
+    foreach (Tile tile in room.Tiles) {
+      GenericPool pool = room.TileAt(tile.x, tile.y + 1) ? this.groundFiller : this.ground;
+      pool.Pop(new Vector3(tile.x, tile.y));
+
+      if (room.Symmetric) {
+        pool.Pop(new Vector3(room.Width - tile.x - 1, tile.y));
+      }
+    }
+
+    foreach (Platform platform in room.Platforms) {
+      Vector3 position = new Vector3(platform.start.x - 0.25f, platform.start.y - 0.25f);
+      this.leftPlatform.Pop(position);
+      position.x += 0.5f;
+      for (int x = 0; x < platform.size; x++, position.x += 0.5f) {
+        this.mainPlatform.Pop(position);
+      }
+      this.rightPlaform.Pop(position);
+
+      // TODO: Symmetric
+    }
+
+    if (room is MonoRoom) {
+      ((MonoRoom)room).CustomRender();
+    }
+  }
+
+  public override void UnloadRoom(Room room) {
+    if (room is MonoRoom) {
+      ((MonoRoom)room).CustomUnload();
+    }
+    this.ground.ReturnAll();
+    this.groundFiller.ReturnAll();
+    this.leftPlatform.ReturnAll();
+    this.mainPlatform.ReturnAll();
+    this.rightPlaform.ReturnAll();
+  }
+
+  [System.Serializable]
+  public struct CustomRoom {
+    public int index;
+    public ManualRoom room;
+  }
+}
