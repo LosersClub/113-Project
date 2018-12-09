751d3d687566988b7b8a3b73d0b49a775ab403b2
jeiipham <jeiipham@yahoo.com>
2018-12-09 01:17:59 -0800

    simple start menu

--- Assets/Runtime/view/StartMenu.cs
+++ Assets/Runtime/view/StartMenu.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement; 
 
 public class StartMenu : MonoBehaviour {
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("PlayerDemo");
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
 
+    public void QuitGame()
+    {
+        Application.Quit(); 
     }
 }
