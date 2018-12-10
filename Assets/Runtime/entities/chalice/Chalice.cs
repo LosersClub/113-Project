be6af93f733dd5b368dfbff36466ec3d8f3f6eea
rajeevgUCI <rajeevg@uci.edu>
2018-12-09 23:54:26 -0800

    Chalice: move smoothly, over time

--- Assets/Runtime/entities/chalice/Chalice.cs
+++ Assets/Runtime/entities/chalice/Chalice.cs
@@ -8,22 +8,27 @@ using UnityEngine.Assertions;
 [RequireComponent(typeof(MovementController))]
 public class Chalice : MonoBehaviour {
 
-  private enum State {Init, Move, FireTell, Fire};
+  private enum State {Init, MoveWait, Moving, FireTell, Fire};
 
   // TODO: refactor state machine code into a separate StandaloneStateMachine class.
   // Note: should use ReadOnlyDictionary instead, but that is only available in .NET 4 (while we
   // are using .NET 3.5):
   private static readonly Dictionary<State, HashSet<State>> StateTransitions = new Dictionary<State, HashSet<State>> {
-    {State.Init, new HashSet<State>{State.Move}},
-    {State.Move, new HashSet<State>{State.FireTell}},
+    {State.Init, new HashSet<State>{State.MoveWait}},
+    {State.MoveWait, new HashSet<State>{State.Moving, State.FireTell}},
+    {State.Moving, new HashSet<State>{State.MoveWait}},
     {State.FireTell, new HashSet<State>{State.Fire}},
-    {State.Fire, new HashSet<State>{State.Move}}
+    {State.Fire, new HashSet<State>{State.MoveWait}}
   };
 
   [SerializeField]
   private GameObject beamEmitterObject;
   [SerializeField]
   private float firingRange = 4f;
+  [SerializeField]
+  private float moveSpeed = 0.2f;
+  // During a MovingState, attempts to move the following distance. May move
+  // less if stopped by a barrier:
   [SerializeField]
   private float moveDistance = 1f;
 
@@ -32,6 +37,7 @@ public class Chalice : MonoBehaviour {
 
   private BeamEmitter beamEmitterComponent;
   private State state = State.Init;
+  private Vector2 moveDirection = Vector2.zero; // will be reassigned before movement
 
   void Start () {
     Assert.IsNotNull(this.beamEmitterObject);
@@ -42,18 +48,22 @@ public class Chalice : MonoBehaviour {
     this.beamEmitterComponent = this.beamEmitterObject.GetComponent<BeamEmitter>();
     Assert.IsNotNull(this.beamEmitterComponent);
 
-    this.ChangeState(State.Move);
+    this.ChangeState(State.MoveWait);
   }
 
   void Update () {
-    if(this.state == State.Move) {
-      this.spriteRendererComponent.color = Color.white;
-    }
-    else if(this.state == State.FireTell) {
+    if(this.state == State.FireTell) {
       this.spriteRendererComponent.color = Color.yellow;
     }
     else if(this.state == State.Fire) {
       this.spriteRendererComponent.color = Color.blue;
+    }
+    else {
+      this.spriteRendererComponent.color = Color.white;
+
+      if(this.state == State.Moving) {
+        this.movementController.Move(this.moveSpeed * Time.deltaTime * this.moveDirection);
+      }
     }
   }
 
@@ -74,8 +84,11 @@ public class Chalice : MonoBehaviour {
     // None currently.
 
     // State entry actions:
-    if(newState == State.Move) {
-      StartCoroutine(MoveCoroutine());
+    if(newState == State.MoveWait) {
+      StartCoroutine(MoveWaitCoroutine());
+    }
+    else if(newState == State.Moving) {
+      StartCoroutine(MoveThenStopCoroutine());
     }
     else if(newState == State.FireTell) {
       StartCoroutine(AimThenFireCoroutine());
@@ -96,19 +109,24 @@ public class Chalice : MonoBehaviour {
   private IEnumerator FireThenMoveStateCoroutine() {
     this.beamEmitterComponent.Fire();
     yield return new WaitUntil(() => !this.beamEmitterComponent.IsFiring);
-    this.ChangeState(State.Move);
+    this.ChangeState(State.MoveWait);
   }
 
-  private IEnumerator MoveCoroutine() {
-    bool done = false;
-    while(!done) {
-      this.movementController.Move((GameManager.Player.transform.position - this.transform.position).normalized * this.moveDistance);
-      yield return new WaitForSeconds(1);
-      if(this.InFiringRange()) {
-        this.ChangeState(State.FireTell);
-        done = true;
-      }
+  private IEnumerator MoveWaitCoroutine() {
+    yield return new WaitForSeconds(1);
+    if(this.InFiringRange()) {
+      this.ChangeState(State.FireTell);
     }
+    else {
+      this.moveDirection = (GameManager.Player.transform.position - this.transform.position).normalized;
+      this.ChangeState(State.Moving);
+    }
+  }
+
+  private IEnumerator MoveThenStopCoroutine() {
+    // Wait, as Movement is performed in update:
+    yield return new WaitForSeconds(this.moveDistance / this.moveSpeed);
+    this.ChangeState(State.MoveWait);
   }
 
   private bool InFiringRange() {
